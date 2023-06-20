27bb3e6b17642901e46cca634f2e420a523d735e
farhanar147 <farhanarrasyid08@gmail.com>
2023-06-20 12:56:22 +0700

    menambahkan p

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>heloo</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque corporis obcaecati officia optio error iusto, expedita accusantium similique, pariatur sequi quia beatae ipsa illo. At suscipit alias distinctio vel sapiente!</p>
+<p>commit</p>
 </body>
 </html>
